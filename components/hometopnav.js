a85651eadf9a0e0a59707d704a88758e432632f3
Khadijah <khadijahlawal99@gmail.com>
2020-12-09 02:14:18 +0400

    completed show page

--- components/hometopnav.js
+++ components/hometopnav.js
@@ -2,21 +2,28 @@ import React from "react";
 import styles from "./hometopnav.module.css";
 import TrendingUpIcon from "@material-ui/icons/TrendingUp";
 import DashboardIcon from "@material-ui/icons/Dashboard";
+import StoreIcon from '@material-ui/icons/Store';
+import Link from "next/link";
 
 function HomeNav() {
   return (
     <div className={styles.header}>
+      <Link href="/home">
       <h1 className={styles.brandTitle}>Reats</h1>
-
+      </Link>
       <div>
         <div className={styles.header__right}>
           <div className={styles.navcomponent}>
-            <TrendingUpIcon fontSize="large" />
-            <p className={styles.navname}>Discover</p>
+            <DashboardIcon className={styles.icon} fontSize="large"/>
+            <p className={styles.navname}>Dashboard</p>
           </div>
           <div className={styles.navcomponent}>
-            <DashboardIcon fontSize="large"/>
-            <p className={styles.navname}>Dashboard</p>
+            <TrendingUpIcon fontSize="large" />
+            <p className={styles.navname}>Portfolio</p>
+          </div>
+          <div className={styles.navcomponent}>
+            <StoreIcon fontSize="large"/>
+            <p className={styles.navname}>Marketplace</p>
           </div>
         </div>
       </div>
--- pages/properties/show.js
+++ pages/properties/show.js
@@ -1,15 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import HomeNav from "../../components/hometopnav";
+import styles from "../../components/showproperty.module.css";
+import PropertyImage from "../../public/images/propimage.jpg";
+import { Button } from "@material-ui/core";
+import Avatar from "../../public/images/user1colored.png";
+import Performance from "../../components/performance";
 
 export class PropertyShow extends Component {
-    render() {
-        return (
-            <div>
-                <HomeNav />
-                <h1>Show Property</h1>
+  render() {
+    return (
+      <div>
+        <HomeNav />
+        <div className={styles.main}>
+          <h3 className={styles.propname}>30b, Roving Heights</h3>
+          <div className={styles.showcase}>
+            <img src={PropertyImage} alt="" />
+          </div>
+          <div className={styles.propinfo}>
+            <div className={styles.investmentinfo}>
+            <h3>Investment Information</h3>
+              <p>
+                Equity Value: $250,000,00 <br/> 
+                Number of shares: 400,000 <br/>
+                Number of shares sold: 200,000 <br/>
+                Available shares for investment: 200,000 <br />
+                Share Price: $2,000
+              </p>
             </div>
-        )
-    }
+            <div className={styles.developerinfo}>
+                 <img src={Avatar} alt="developer image" />
+                <h3>Realington Realty</h3>
+            </div>
+          </div>
+         <div className={styles.performance}>
+             <h3>Performance </h3>
+             <Performance />
+         </div>       
+          {/* Performance line chart */}
+          {/* Earnings bar graph */}
+          {/* List of investors */}
+          <div className={styles.investors}>
+             <h3>Performance </h3>
+         </div> 
+          <div className={styles.action__buttons}>
+          <Button> Invest </Button>
+          <Button> Rent </Button>
+        </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default PropertyShow
+export default PropertyShow;
